86ee0d59d91eab5601684a01865b65a824930765
Lenny Makori <makorilenny28@gmail.com>
2020-05-31 19:30:47 +0300

    create a testcase for the profile model and include the setup test

--- instamine/tests.py
+++ instamine/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Profile, Image
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+
+    def setUp(self):
+        self.user = Profile(profile_pic='profile.jpg', bio='Big man Bazuu')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, Profile))
