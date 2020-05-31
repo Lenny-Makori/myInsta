96413d3532bea88371dfe72fed06e79b756e2eb2
Lenny Makori <makorilenny28@gmail.com>
2020-05-31 19:00:55 +0300

    create an image profile

--- instamine/models.py
+++ instamine/models.py
@@ -4,3 +4,10 @@ from django.db import models
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to='image/')
     bio = models.TextField()
+
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='image/')
+    image_name = models.CharField(max_length=60)
+    image_caption = models.CharField(max_length=60)
+    profile = models.ForeignKey(Profile)
