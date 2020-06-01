89c0769c55a9b7e42f231bfedd14c443bc8952c3
Lenny Makori <makorilenny28@gmail.com>
2020-06-01 17:33:41 +0300

    create new image view function

--- instamine/views.py
+++ instamine/views.py
@@ -9,6 +9,8 @@ def index(request):
     return render(request, 'mainview/timeline.html', {"image_display":image_display})
 
 def search_results(request):
+    if 'userToFollow' in request.GET and request.GET['userToFollow']:
+        search_term = request.GET.get("userToFollow")
 
     return render(request, 'mainview/search.html')
 
@@ -23,4 +25,18 @@ def profile(request):
     user_profile = Profile.get_profile(id=user_id)
 
     return render(request, 'profile.html', {'profile':profile})
+
+@login_required(login_url='/accounts/login/')
+def new_image(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageForm(request.POST, request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('index')
+    else:
+        form = ImageForm()
+    return render(request,'new_image.html',{'form':form})
 
