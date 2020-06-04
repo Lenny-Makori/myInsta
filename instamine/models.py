1f54aa639baf22d1aa8cbc4709bf1bbe3f6894ee
Lenny Makori <makorilenny28@gmail.com>
2020-06-04 14:48:22 +0300

    enable authentication of a new user

--- instamine/models.py
+++ instamine/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
 class Profile(models.Model):
@@ -21,6 +23,16 @@ class Profile(models.Model):
         searched_user = cls.objects.filter(username__icontains=search_term)
 
         return searched_user
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
 
 
 class Image(models.Model):
--- instamine/views.py
+++ instamine/views.py
@@ -3,6 +3,7 @@ from .models import Image, Profile
 from .forms import ImageForm, ProfileForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 @login_required(login_url='/accounts/login/')
@@ -11,6 +12,7 @@ def index(request):
 
     return render(request, 'mainview/timeline.html', {"image_display":image_display})
 
+@login_required(login_url='/accounts/login/')
 def search_results(request):
     if 'userToFollow' in request.GET and request.GET['userToFollow']:
         search_term = request.GET.get("userToFollow")
@@ -42,19 +44,22 @@ def profile(request,user_id):
     return render(request, 'profile.html', {'user_profile':user_profile})
 
 @login_required(login_url='/accounts/login/')
+# @transaction.atomic
 def update_profile(request):
-    current_user = request.user
     if request.method == 'POST':
-        form = ProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            profile = form.save(commit=False)
-            profile.user = current_user
+        user_form = UserCreationForm(request.POST, instance=request.user)
+        profile_form = ProfileForm(request.POST, request.FILES)
+        if user_form.is_valid() and profile_form.is_valid():
+            current_user = user_form.save()
+            # profile_form.save()
+            profile = profile_form.save(commit=False)
+            profile.user = current_user.id
             profile.save()
         return redirect('profileview')
     else:
         form = ProfileForm()
 
-    return render(request, 'profile_edit.html', {"form":form})
+    return render(request, 'profile_edit.html', {'form':profile_form})
 
 @login_required(login_url='/accounts/login/')
 def new_image(request):
